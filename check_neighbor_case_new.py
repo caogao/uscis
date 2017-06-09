d5acfa7aacaf4c82dd1c817c3bcc23251186090a
caogao <caogao@umich.edu>
2017-06-09 16:48:50 -0400

    commit

--- check_neighbor_case_new.py
+++ check_neighbor_case_new.py
@@ -16,24 +16,20 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import requests
-import re
 import sys
-import os
-import pycurl, json
-import cStringIO
 import argparse
 import json
 import yaml
 import datetime
 import time
 import multiprocessing
-import io
 from multiprocessing import Value, Lock, Manager
-from tabulate import tabulate
 from bs4 import BeautifulSoup
 import certifi
 
 CPU_CORES = multiprocessing.cpu_count()
+RETRY_THRESHOLD1 = 3
+RETRY_THRESHOLD2 = 6
 
 def cmdArgumentParser():
     parser = argparse.ArgumentParser()
@@ -52,56 +48,60 @@ def cmdArgumentParser():
 
 def get_result(case_num,prefix,verbose):
     retries = 0
+    url = 'https://egov.uscis.gov/casestatus/mycasestatus.do'
+    case_num = prefix + str(case_num)
+    if verbose:
+        print "case_num: ", case_num
+
     while True:
         info = {}
         result=''
-        url = 'https://egov.uscis.gov/casestatus/mycasestatus.do'
-        case_num = prefix + str(case_num)
-        print "case_num: ", case_num
         response = requests.post(url, data={"appReceiptNum": case_num})
-        if response.status_code != 200:
-            print "USCIS format is incorrect"
-            time.sleep(120) 
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content)
+            mycase_txt = soup.findAll("div", { "class" : "current-status-sec" })
+
+            for i in mycase_txt:
+                result = i.get_text() 
+                if "Your Current Status" in result:
+                    result = result.replace("Your Current Status:", "").strip("\n\r\t+ ")
+                    info[case_num] = {}
+                    info[case_num]["Status"] = result
+                    return info
+
+        if retries < RETRY_THRESHOLD1:
+            print "USCIS format is incorrect: retry after 15 secs"
+            time.sleep(15) 
             retries += 1
-            continue    
-            
-        soup = BeautifulSoup(response.content)
-        mycase_txt = soup.findAll("div", { "class" : "current-status-sec" })
-
-        for i in mycase_txt:
-            result = i.get_text() 
-            if "Your Current Status" in status:
-                status = status.replace("Your Current Status:", "").strip("\n\r\t+ ")
-                info[case_num] = {}
-                return info
-
-        if retries < 2:
-            print "USCIS format is incorrect"
-            time.sleep(120) 
+        elif retries < RETRY_THRESHOLD2:
+            print "USCIS format is incorrect: possible IP ban, retry after 300 secs"
+            time.sleep(300) 
             retries += 1
         else:
-            print "retry too many times, exiting"
+            print "retried too many times, printing website content and exiting"
             print soup
             sys.exit()
 
-def get_batch_pair(interval,case_s,case_e):
-  batch = {}
-  info = []
-  for i in range(interval / CPU_CORES):
-    s = CPU_CORES * i + case_s
-    e = s + CPU_CORES - 1
-    batch = {
-      "start":s,
-      "end": e
-    }
-    info.append(batch)
-  return info
+def get_batch_pair(interval,case_s,case_e,skip):
+    batch = {}
+    info = []
+    per_cpu = (interval + CPU_CORES - 1) / CPU_CORES
+    for i in range(CPU_CORES - 1):
+        batch = {
+          "start": case_s + i * per_cpu,
+          "end": case_s + (i + 1) * per_cpu,
+          "skip": skip
+        }
+        info.append(batch)
+    info.append({"start": case_s + (CPU_CORES - 1) * per_cpu, "end": case_e, "skip": skip})
+    return info
 
 def query_website(ns,batch_result,prefix,lock,verbose):
     local_result = []
     if verbose:
-        print 's is %d, e is %d'%(int(batch_result['start']),int(batch_result['end']))
-    for case_n in range(int(batch_result['start']),int(batch_result['end'])):
+        print "start is %d, end is %d"%(int(batch_result['start']),int(batch_result['end']))
+        print "current pid: ", multiprocessing.current_process()
+    for case_n in range(int(batch_result['start']),int(batch_result['end']), int(batch_result['skip'])):
         local_result.append(get_result(case_n,prefix,verbose))
 
     ## local_result: list of dicts. Each dict contains one case_num
@@ -110,11 +110,6 @@ def query_website(ns,batch_result,prefix,lock,verbose):
     lock.release()
 
 def main():
-    lock = Lock()
-    jobs = []
-    mgr = Manager()
-    ns = mgr.Namespace()
-    
     args = cmdArgumentParser()
     case_numberic = int(args.end_num[3:])
     prefix = args.end_num[:3]
@@ -130,16 +125,21 @@ def main():
         skip = 25
 
     now = datetime.datetime.now()
-    result_file = open('data-%s-%s-%s.yml' % (str(overall_start), str(overall_end), now.strftime("%m-%d")), 'w') 
+    result_file = open('data-%s-%s-%s.log' % (str(overall_start), str(overall_end), now.strftime("%m-%d")), 'w', 0) 
+        # 0 is buffer size. This means the content will be flushed to file instantly. 
+
     for start in range(overall_start, overall_end, interval):
-        final_result = []
-        ns.df = final_result
-
-        rmnder = interval % CPU_CORES
         end = start + interval
+        final_result = []
 
-        if False:
-            batch_result = get_batch_pair(interval,start,end)
+        if (interval / skip) > (CPU_CORES * 5):
+            lock = Lock()
+            jobs = []
+            mgr = Manager()
+            ns = mgr.Namespace()
+    
+            ns.df = final_result
+            batch_result = get_batch_pair(interval,start,end,skip)
 
             for i in range(len(batch_result)):
                 p = multiprocessing.Process(target=query_website,args=(ns,batch_result[i],prefix,lock,args.verbose,))
@@ -156,17 +156,17 @@ def main():
 
         ## parse
         total_case_num = 0
-        recv_case_num = 0
+        mailed_case_num = 0
         for result_dict in final_result:              ## dict:{case_num: {type, status, recv} 
             for key in result_dict.values(): 
                 if key['Status'][:15] == "Card Was Mailed":
-                    recv_case_num += 1
+                    mailed_case_num += 1
                 total_case_num += 1
 
         if total_case_num == 0:
             continue
-        print "start_num: %d, recv_case_num: %d, recv_rate: %.2f%%" % (start, recv_case_num, 100.0 * recv_case_num / total_case_num)
-        result_file.write("start_num: %d, recv_rate: %.2f%%" % (start, 100.0 * recv_case_num / total_case_num))
+        print "start_num: %d, mailed_case_num: %d, mailed_rate: %.0f%%" % (start, mailed_case_num, 100.0 * mailed_case_num / total_case_num)
+        result_file.write("start_num: %d, mailed_rate: %.0f%%\n" % (start, 100.0 * mailed_case_num / total_case_num))
 
         json_type = json.dumps(final_result,indent=4)
         with open('result/data-%s-%s-%s.yml' % (str(start), str(end), now.strftime("%m-%d")), 'w') as outfile:
