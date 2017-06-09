82068914d1eabf9211e3427b47fea71f1cd49c18
caogao <caogao@umich.edu>
2017-06-08 22:38:55 -0400

    newer request procedure

--- check_neighbor_case_new.py
+++ check_neighbor_case_new.py
@@ -55,50 +55,34 @@ def get_result(case_num,prefix,verbose):
     while True:
         info = {}
         result=''
-        buf = cStringIO.StringIO()
         url = 'https://egov.uscis.gov/casestatus/mycasestatus.do'
         case_num = prefix + str(case_num)
         print "case_num: ", case_num
-        c = pycurl.Curl()
-        c.setopt(c.URL, url)
-        c.setopt(c.POSTFIELDS, 'appReceiptNum=%s'%case_num)
-        c.setopt(c.WRITEFUNCTION, buf.write)
-        c.setopt(c.CAINFO, certifi.where())
-        c.perform()
-
-        soup = BeautifulSoup(buf.getvalue(),"html.parser")
-        mycase_txt = soup.findAll("div", { "class" : "rows text-center" })
+        response = requests.post(url, data={"appReceiptNum": case_num})
+        if response.status_code != 200:
+            print "USCIS format is incorrect"
+            time.sleep(120) 
+            retries += 1
+            continue    
+            
+        soup = BeautifulSoup(response.content)
+        mycase_txt = soup.findAll("div", { "class" : "current-status-sec" })
 
         for i in mycase_txt:
-            result=result+i.text
+            result = i.get_text() 
+            if "Your Current Status" in status:
+                status = status.replace("Your Current Status:", "").strip("\n\r\t+ ")
+                info[case_num] = {}
+                return info
 
-        result = result.split('\n')
-        buf.close()
-        try:
-            details = result[2].split(',')
-            recv_date = get_case_receive_date(details)
-            case_type = get_case_type(result[2])
-            info[case_num] = {}
-            info[case_num]['Status'] = result[1]
-            info[case_num]['Received'] = recv_date
-
-            if verbose:
-                print info
-
-        except Exception as e:      ## re-try
-            if retries < 2:
-                print "USCIS format is incorrect"
-                time.sleep(120) 
-                retries += 1
-                continue    
-            else:
-                print "retry too many times, exiting"
-                print soup
-                sys.exit()
-
-        break
-
-    return info
+        if retries < 2:
+            print "USCIS format is incorrect"
+            time.sleep(120) 
+            retries += 1
+        else:
+            print "retry too many times, exiting"
+            print soup
+            sys.exit()
 
 def get_batch_pair(interval,case_s,case_e):
   batch = {}
@@ -114,105 +98,79 @@ def get_batch_pair(interval,case_s,case_e):
   return info
 
 def query_website(ns,batch_result,prefix,lock,verbose):
-  local_result = []
-  if verbose:
-    print 's is %d, e is %d'%(int(batch_result['start']),int(batch_result['end']))
-  for case_n in range(int(batch_result['start']),int(batch_result['end'])):
-    local_result.append(get_result(case_n,prefix,verbose))
+    local_result = []
+    if verbose:
+        print 's is %d, e is %d'%(int(batch_result['start']),int(batch_result['end']))
+    for case_n in range(int(batch_result['start']),int(batch_result['end'])):
+        local_result.append(get_result(case_n,prefix,verbose))
 
-  ## local_result: list of dicts. Each dict contains one case_num
-  lock.acquire()
-  ns.df = ns.df + local_result
-  lock.release()
-
-def get_case_type(line):
-
-  iCase = re.search("\w*I-\w*", line)
-  crCase = re.search("\w*CR-\w*", line)
-  irCase = re.search("\w*IR-\w*", line)
-  
-  if iCase:
-    return iCase.group(0)
-  elif crCase:
-    return crCase.group(0)
-  elif irCase:
-    return irCase.group(0)
-  else:
-    return 'None Case'
-
-def get_case_receive_date(details):
-  year = str(details[1][1:])
-
-  if year.isdigit():
-    recv_date = details[0][3:] + ' ' + details[1][1:]
-  else:
-    recv_date = None
-
-  return recv_date
+    ## local_result: list of dicts. Each dict contains one case_num
+    lock.acquire()
+    ns.df = ns.df + local_result
+    lock.release()
 
 def main():
-  lock = Lock()
-  jobs = []
-  mgr = Manager()
-  ns = mgr.Namespace()
-  
-  args = cmdArgumentParser()
-  case_numberic = int(args.end_num[3:])
-  prefix = args.end_num[:3]
-  if not args.dryrun:
-      overall_start = case_numberic - args.range
-      overall_end = case_numberic
-      interval = args.interval
-      skip = args.skip
-  else:
-      overall_start = case_numberic - 500
-      overall_end = case_numberic
-      interval = 100
-      skip = 25
-
-  now = datetime.datetime.now()
-  result_file = open('data-%s-%s-%s.yml' % (str(overall_start), str(overall_end), now.strftime("%m-%d")), 'w') 
-  for start in range(overall_start, overall_end, interval):
-    final_result = []
-    ns.df = final_result
-
-    rmnder = interval % CPU_CORES
-    end = start + interval
-
-    if False:
-      batch_result = get_batch_pair(interval,start,end)
-
-      for i in range(len(batch_result)):
-        p = multiprocessing.Process(target=query_website,args=(ns,batch_result[i],prefix,lock,args.verbose,))
-        jobs.append(p)
-        p.start()
-      for job in jobs:
-        job.join()
-
-      final_result = ns.df
-
+    lock = Lock()
+    jobs = []
+    mgr = Manager()
+    ns = mgr.Namespace()
+    
+    args = cmdArgumentParser()
+    case_numberic = int(args.end_num[3:])
+    prefix = args.end_num[:3]
+    if not args.dryrun:
+        overall_start = case_numberic - args.range
+        overall_end = case_numberic
+        interval = args.interval
+        skip = args.skip
     else:
-        for i in range(start,end,skip):
-            print "case_num: ", i
-            final_result.append(get_result(i,prefix,args.verbose))
+        overall_start = case_numberic - 500
+        overall_end = case_numberic
+        interval = 100
+        skip = 25
 
-    ## parse
-    total_case_num = 0
-    recv_case_num = 0
-    for result_dict in final_result:              ## dict:{case_num: {type, status, recv} 
-        for key in result_dict.values(): 
-            if key['Status'][:15] == "Card Was Mailed":
-                recv_case_num += 1
-            total_case_num += 1
+    now = datetime.datetime.now()
+    result_file = open('data-%s-%s-%s.yml' % (str(overall_start), str(overall_end), now.strftime("%m-%d")), 'w') 
+    for start in range(overall_start, overall_end, interval):
+        final_result = []
+        ns.df = final_result
 
-    if total_case_num == 0:
-        continue
-    print "start_num: %d, recv_rate: %.2f%%" % (start, 100.0 * recv_case_num / total_case_num)
-    result_file.write("start_num: %d, recv_rate: %.2f%%" % (start, 100.0 * recv_case_num / total_case_num))
+        rmnder = interval % CPU_CORES
+        end = start + interval
 
-    json_type = json.dumps(final_result,indent=4)
-    with open('result/data-%s-%s-%s.yml' % (str(start), str(end), now.strftime("%m-%d")), 'w') as outfile:
-        yaml.dump(yaml.load(json_type), outfile, allow_unicode=True)
+        if False:
+            batch_result = get_batch_pair(interval,start,end)
+
+            for i in range(len(batch_result)):
+                p = multiprocessing.Process(target=query_website,args=(ns,batch_result[i],prefix,lock,args.verbose,))
+                jobs.append(p)
+                p.start()
+            for job in jobs:
+                job.join()
+
+            final_result = ns.df
+
+        else:
+            for i in range(start,end,skip):
+                final_result.append(get_result(i,prefix,args.verbose))
+
+        ## parse
+        total_case_num = 0
+        recv_case_num = 0
+        for result_dict in final_result:              ## dict:{case_num: {type, status, recv} 
+            for key in result_dict.values(): 
+                if key['Status'][:15] == "Card Was Mailed":
+                    recv_case_num += 1
+                total_case_num += 1
+
+        if total_case_num == 0:
+            continue
+        print "start_num: %d, recv_case_num: %d, recv_rate: %.2f%%" % (start, recv_case_num, 100.0 * recv_case_num / total_case_num)
+        result_file.write("start_num: %d, recv_rate: %.2f%%" % (start, 100.0 * recv_case_num / total_case_num))
+
+        json_type = json.dumps(final_result,indent=4)
+        with open('result/data-%s-%s-%s.yml' % (str(start), str(end), now.strftime("%m-%d")), 'w') as outfile:
+            yaml.dump(yaml.load(json_type), outfile, allow_unicode=True)
 
 if __name__ == "__main__":
     main()
